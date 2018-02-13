589e3e548715c8f8b5f4a99787124212411fff68
Artaria <mahdi.niknami@gmail.com>
2018-02-13 19:27:20 +0330

    change index to home in views/index.view.php

--- views/index.view.php
+++ views/index.view.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
-            <h2>Index Page</h2>
+            <h2>Home Page</h2>
             <a href="login.php">login</a>
             <a href="register.php">register</a>
         </div>
